d448ff4d86eeaa6dcddf13ccaf5aacdb813989b5
TahshinSharon <atsharon88@gmail.com>
2025-09-17 00:30:04 +0600

    start java again

---
+++ AdvanceJava.java
@@ -0,0 +1,19 @@
+/*
+ * ArrayList,List
+ * Map,HashMap
+ * Optionals
+ * Streams
+ * Interfaces
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+public class AdvanceJava{
+    static public void main(String[]args){
+        List<Object>arr=new ArrayList<>();
+        Object [] arr1= {"abc",2,3};
+        for(int i=0;i<arr1.length;i++){
+            System.out.println(arr1[i]);
+        }
+    }
+}
